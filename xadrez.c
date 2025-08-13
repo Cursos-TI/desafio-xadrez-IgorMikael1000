49f369b2757615243420f6043734260597d60973
IgorMikael1000 <igormikael_9@hotmail.com>
2025-08-13 22:52:50 +0000

    ultimo desafio xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,36 +1,56 @@
 #include <stdio.h>
 
+//RECURSIVIDADE PARA MOVIMENTO DA TORRE
+void movimentoTorre (int t){
+   if(t > 0){
+       movimentoTorre(t - 1);
+       printf("Direita\n");
+   }
+}
+
+//RECURSIVIDADE PARA MOVIMENTO DO BISPO
+void movimentoBispo (int b){
+
+    int i, j;
+
+    for(i = 1; i <= 5; i++){
+        for(j = 1; j <= 1; j++){
+                printf("Direita\n");
+        }
+        printf("Cima\n")
+    }
+
+}
+
+//RECURSIVIDADE PARA MOVIMENTO DA RAINHA
+void movimentoRainha (int r){
+    if(r > 0){
+        movimentoRainha(r - 1);
+        printf("Esquerda\n");
+    }
+}
+
+
 int main() {
     
     //DECLARAÇÃO DE VARIÁVEIS
-    int torre = 1, bispo = 1, cavalo = 1;
+    int torre = 1, bispo = 1, rainha = 1, cavalo = 1;
 
     //REALIZA O MOVIMENTO DA TORRE
-    while(torre <= 5){
-        printf("Direita\n");
-        torre++;
-    }
+    movimentoTorre(torre);
     
     //REALIZA O MOVIMENTO DO BISPO
-    do{
-        printf("Cima, Direita\n");
-        bispo++;
-
-    } while(bispo <= 5);
+    movimentoBispo(bispo);
 
     //REALIZA O MOVIMENTO DA RAINHA
-    for(int rainha = 1; rainha <= 8; rainha++){
-        printf("Esquerda\n");
-    }
+    movimentoRainha(rainha);
     
     //REALIZA O MOVIMENTO DO CAVALO
-    while(cavalo == 1){
-
-        for(cavalo = 1; cavalo <= 2; cavalo++){
-
-            printf("Baixo\n");
+    for(int i = 1, j = 1; i <= 1 && j <= 1; i++, j++){
+        for(int k = 1, l = 1; i <= 2 && j <= 2; i++, j++){
+            printf("Cima\n");
         }
-        printf("Esquerda\n");
+        printf("Direita\n")
     }
 
     return 0;
